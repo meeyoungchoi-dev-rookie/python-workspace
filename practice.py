0b8f06eedc9eca12ed7380d34ad61017824ae11a
meeyoungchoi <goodlife1359@gmail.com>
2022-07-16 14:02:31 +0900

    python 문자 자료형 실습

--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 print(5)
 print(-10)
 print(3.14)
@@ -5,3 +8,8 @@ print(1000)
 print(5 + 3)
 print(2 * 8)
 print(3* (3 + 1))
+
+
+print('풍선')
+print('나비')
+print("ㅋ" * 5)
