8a004328cdfc57e88b75bbee2dc2a74f49b05811
meeyoungchoi <goodlife1359@gmail.com>
2022-07-17 14:35:25 +0900

    boolean 자료형

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-workspace
-python
+## python
+
+### 1. 자료형
++ 숫자 자료형
++ 문자열 자료형
++ boolean 자료형
--- practice.py
+++ practice.py
@@ -13,3 +13,11 @@ print(3* (3 + 1))
 print('풍선')
 print('나비')
 print("ㅋ" * 5)
+
+# 참 / 거짓
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not (5 > 10)) # false의 반대이므로 true
+
