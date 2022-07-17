457e78a3e6c7cfaf8340fd6c5648dc8c2e03df9f
meeyoungchoi <goodlife1359@gmail.com>
2022-07-17 17:06:14 +0900

    랜덤함수

--- section2.py
+++ section2.py
@@ -46,3 +46,13 @@ print(floor(4.99))
 print(ceil(3.14))
 print(sqrt(16))
 
+
+from random import *
+print(random())
+print(random() * 10)
+print(int(random() * 10))
+print(int(random() * 10) + 1)
+
+print(int(random() * 45) + 1)
+
+print(randrange(1, 46))
