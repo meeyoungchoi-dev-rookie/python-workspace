c2dd54df330bff52a603730a07dbc2d557997e91
meeyoungchoi <goodlife1359@gmail.com>
2022-07-17 15:04:16 +0900

    변수 관련 Quiz

--- practice.py
+++ practice.py
@@ -30,9 +30,11 @@ is_adult = age >= 3
 
 print("우리집" +  animal + " 의 이름은" +  name + "에요")
 print(name + "는"  + str(age) + "살이며," +  hobby + " 을 아주 좋아해요")
-print("연탄이는 어른일까요?" + is_adult)
+print("연탄이는 어른일까요?" + str(is_adult))
 
-
+# Quiz
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
 
 
 
