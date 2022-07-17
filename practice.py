b9b62f1ddb80b200925ea419a138c9caa147f995
meeyoungchoi <goodlife1359@gmail.com>
2022-07-17 14:59:45 +0900

    변수

--- practice.py
+++ practice.py
@@ -21,3 +21,27 @@ print(True)
 print(False)
 print(not (5 > 10)) # false의 반대이므로 true
 
+# 애완동물을 소개해 주세요
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3 
+
+print("우리집" +  animal + " 의 이름은" +  name + "에요")
+print(name + "는"  + str(age) + "살이며," +  hobby + " 을 아주 좋아해요")
+print("연탄이는 어른일까요?" + is_adult)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
