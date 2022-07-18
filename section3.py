8d5062d407453efb2a336f1ccbd94ec26f030307
meeyoungchoi <goodlife1359@gmail.com>
2022-07-18 22:26:24 +0900

    문자열 처리함수 - replace, find , index , count

--- section3.py
+++ section3.py
@@ -21,3 +21,21 @@ print("일: " + jumin[4:6])
 print("생년월일 : " + jumin[:6])
 print("뒤 7자리 : " + jumin[7:]) # 7인덱스 부터 끝까지
 print("뒤 7자리 (뒤에부터) : " + jumin[-7:]) # 맨 뒤에서 7번째부터 끝까지
+
+# 문자열 처리 함수
+python = "Python is Amazing"
+print(python.lower())
+print(python[0].isupper()) # 0인덱스의 값이 대문자인지 판별
+print(len(python))
+print(python.replace("Python", "Java"))
+
+index = python.index("n")
+print(index)
+index = python.index("n", index + 1) # 첫번째 n이 나온 인덱스 위치부터 계산해서 또 n이 나오는 위치를 찾는다
+print(index)
+
+print(python.find("n"))
+print(python.find("Java")) # 원하는 값을 찾을 수 없을때 : -1
+#print(python.index("Java")) # 원하는 값을 찾을 수 없는 경우 ValueError 발생
+
+print(python.count("n"))
